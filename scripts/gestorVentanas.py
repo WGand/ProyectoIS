245de7197aa1e495ad7af2f23d4ef4ee612cbce5
Will <wgandino>
2020-05-30 06:12:58 -0400

    Actualización
    Creación de gestorVentas, que contiene las clases de las ventanas creadas hasta el momento y su manejo

---
+++ scripts/gestorVentanas.py
@@ -0,0 +1,55 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QDialog
+from ventanaMenu import Ui_MainWindow
+from ventanaGestionarProducto import Ui_Dialog
+from ventanaListarInventario import Ui_Dialog
+#Editado con Sublime Text
+class ventanaListarInventario(QDialog):
+    def __init__(self):
+        super(ventanaListarInventario, self).__init__() #redefinicion de la clase QDialog con las necesidades de ventanaListarInventariopy, IDEM a todas las ventanas
+        self.ui = Ui_Dialog() #1- descarga de la interfaz 
+        self.ui.setupUi(self) #2- carga de la interfaz sobre el objeto. 1 y 2 IDEM todas las ventanas
+        self.setWindowTitle("Listar Inventario")
+        self.setWindowModality(2) #Detiene toda la actividad en las otras ventanas, ejemplo, salir pulsando la "x", IDEM a todas las ventanas
+
+class ventanaGestionarProducto(QDialog):
+    def __init__(self):
+        super(ventanaGestionarProducto, self).__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.setWindowTitle("Gestionar Producto")
+        self.setWindowModality(2)
+
+class ventanaRegistrarVenta(QDialog):
+    def __init__(self):
+        super(ventanaRegistrarVenta, self).__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.setWindowTitle("Registrar Venta")
+        self.setWindowModality(2)
+
+class ventanaMenu(QMainWindow):
+    def __init__(self):
+        super(ventanaMenu, self).__init__() 
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowTitle("Menu")
+        self.ui.botonGestionarProducto.clicked.connect(self.irGestionarProducto) #conexion entre el boton y su ventana, mas accion. IDEM a todos los botones
+        self.ui.botonListarInventario.clicked.connect(self.irListarInventario)
+        self.ui.botonRegistrarVenta.clicked.connect(self.irRegistrarVenta)
+        self.ui.botonSalir.clicked.connect(self.salir)
+
+    def irGestionarProducto(self):
+        self.ventana_GestionarProducto = ventanaGestionarProducto() #ventana_GestionarProducto en vez de ventanaGestionarProducto para confusion en el interpretador, IDEM a todas las ventanas
+        self.ventana_GestionarProducto.show()
+
+    def irListarInventario(self):
+        self.ventana_ListarInventario = ventanaListarInventario()
+        self.ventana_ListarInventario.show()
+
+    def irRegistrarVenta(self):
+        self.ventana_RegistrarVenta = ventanaRegistrarVenta()
+        self.ventana_RegistrarVenta.show()
+
+    def salir(self):
+        self.close()
