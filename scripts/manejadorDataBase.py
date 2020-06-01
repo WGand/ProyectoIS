a51886e5391e0d51509b70bdf08ed43c74917b73
wgan <wgandino>
2020-06-01 00:42:57 -0400

    //

--- scripts/manejadorDataBase.py
+++ scripts/manejadorDataBase.py
@@ -10,8 +10,8 @@ class ConexionDataBase:
         
         ConexionDataBase.db.setHostName("localhost")
         ConexionDataBase.db.setPort(5432)
-        ConexionDataBase.db.setDatabaseName("inventarioabasto")
-        ConexionDataBase.db.setUserName("froilanroac")
+        ConexionDataBase.db.setDatabaseName("postgres")
+        ConexionDataBase.db.setUserName("postgres")
         ConexionDataBase.db.setPassword("")
 
     def openDB(self):
@@ -19,8 +19,7 @@ class ConexionDataBase:
 
 
     #Select
-    def validarCliente(self,cedula):
-        'Devuelve True si esta en la DB'
+    def validarCliente(self,cedula): #Devuelve True si esta en la DB
         sql = "SELECT FROM cliente WHERE cedula = " + str(cedula) + ";"
         query = QSqlQuery()
         query.exec_(sql)
@@ -29,8 +28,7 @@ class ConexionDataBase:
         else:
             return False
 
-    def validarMarca(self,nombre):
-        'Devuelve True si esta en la DB'
+    def validarMarca(self,nombre): #Devuelve True si esta en la DB
         sql = "SELECT FROM marca WHERE nombre = '" + str(nombre) + "';"
         query = QSqlQuery()
         query.exec_(sql)
@@ -39,8 +37,7 @@ class ConexionDataBase:
         else:
             return False
 
-    def validarProducto(self,nombre):
-        'Devuelve True si esta en la DB'
+    def validarProducto(self,nombre): #Devuelve True si esta en la DB
         sql = "SELECT FROM producto WHERE nombre = '" + str(nombre) + "';"
         query = QSqlQuery()
         query.exec_(sql)
@@ -49,8 +46,7 @@ class ConexionDataBase:
         else:
             return False
 
-    def validarProveedor(self,nombre):
-        'Devuelve True si esta en la DB'
+    def validarProveedor(self,nombre): #Devuelve True si esta en la DB
         sql = "SELECT FROM proveedor WHERE nombre = '" + str(nombre) + "';"
         query = QSqlQuery()
         query.exec_(sql)
@@ -59,8 +55,7 @@ class ConexionDataBase:
         else:
             return False
 
-    def validarVendedor(self,cedula):
-        'Devuelve True si esta en la DB'
+    def validarVendedor(self,cedula): #Devuelve True si esta en la DB
         sql = "SELECT FROM vendedor WHERE cedula = " + str(cedula) + ";"
         query = QSqlQuery()
         query.exec_(sql)
@@ -71,48 +66,41 @@ class ConexionDataBase:
 
     #Insert
     def insertCliente(self, cedula, telefono, nombre):
-
         if (self.validarCliente(cedula) == False):
             sql = "INSERT INTO cliente(cedula, telefono, nombre) VALUES ("+str(cedula)+", "+str(telefono)+", '"+str(nombre)+"');"
             query = QSqlQuery()
             query.exec_(sql)
     
     def insertMarca(self, nombre):
-
         if (self.validarMarca(nombre) == False):
             sql = "INSERT INTO marca(nombre) VALUES ('"+str(nombre)+"');"
             query = QSqlQuery()
             query.exec_(sql)
     
     def insertProducto(self, nombre, cantidad, precio, proveedor_id, marca_id):
-
         if (self.validarProducto(nombre) == False):
             sql = "INSERT INTO producto(nombre, cantidad, precio, proveedor_id, marca_id) VALUES ('"+str(nombre)+"',"+str(cantidad)+","+str(precio)+","+str(proveedor_id)+","+str(marca_id)+");"
             query = QSqlQuery()
             query.exec_(sql)
 
     def insertProveedor(self, nombre):
-
         if (self.validarProveedor(nombre) == False):
             sql = "INSERT INTO proveedor(nombre) VALUES ('"+str(nombre)+"');"
             query = QSqlQuery()
             query.exec_(sql)
 
     def insertVendedor(self, cedula, telefono, nombre, proveedor_id):
-
         if (self.validarVendedor(cedula) == False):  
             sql = "INSERT INTO vendedor(cedula, telefono, nombre,proveedor_id) VALUES ("+str(cedula)+", "+str(telefono)+", '"+str(nombre)+"', "+str(proveedor_id)+");"
             query = QSqlQuery()
             query.exec_(sql)
     
     def insertVenta(self, monto, cliente_id):
-
         sql = "INSERT INTO venta(monto, cliente_id) VALUES ("+str(monto)+", "+str(cliente_id)+");"
         query = QSqlQuery()
         query.exec_(sql)
     
     def insertVentaProducto(self, cantidad, producto_id, venta_id):
-
         sql = "INSERT INTO venta_producto(cantidad, producto_id, venta_id) VALUES ("+str(cantidad)+", "+str(producto_id)+", "+str(venta_id)+");"
         query = QSqlQuery()
         query.exec_(sql)
@@ -222,6 +210,5 @@ class ConexionDataBase:
 #Main
 conector = ConexionDataBase()
 conector.openDB()
-
 
 
