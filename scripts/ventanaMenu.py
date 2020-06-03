278228efb354f3fd96c3c82aa80974667d6f1252
gabriel <costanzag1999@gmail.com>
2020-06-03 18:48:49 -0400

    LIsto

--- scripts/ventanaMenu.py
+++ scripts/ventanaMenu.py
@@ -19,7 +19,6 @@ class Ui_MainWindow(object):
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(0, 0, 791, 551))
         self.label.setText("")
-        self.label.setPixmap(QtGui.QPixmap("../assets/freeman.jpg"))
         self.label.setObjectName("label")
         self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 791, 551))
